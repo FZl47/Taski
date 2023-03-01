b27a969010a5885a9d16b3d9c3a049488edc1db0
Fazel Momeni <wwwfazel88@gmail.com>
2023-03-01 08:21:48 +0330

    Config project

---
+++ config/urls.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from django.urls import path as _path, re_path, include
+from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+def path(pth, *args, **kwargs):
+    return _path(f"{settings.NAME_POINT_API}/{pth}", *args, **kwargs)
+
+
+urlpatterns = [
+    path('u/', include('account.urls')),
+    path('', include('public.urls')),
+    path('admin/', admin.site.urls),
+]
+
+
+
+# Drf-yasg - DOC
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Taski API",
+        default_version=settings.API_VERSION,
+        description="API Task manager Business",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(url="https://github.com/FZl47/Taski"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+urlpatterns.extend([
+    re_path(r'swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+])
+
+# Init Swagger(Drf-yasg)
+from core.mixins import swagger
+swagger._init()
