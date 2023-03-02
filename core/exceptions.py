c98a5b13c6718935953881c25686615e75ef1151
Fazel Momeni <wwwfazel88@gmail.com>
2023-03-02 09:43:43 +0330

    Exception core

--- core/exceptions.py
+++ core/exceptions.py
@@ -1,5 +1,25 @@
 from rest_framework.exceptions import APIException
 from rest_framework import status
+from rest_framework.views import exception_handler
+
+
+def custom_exception_handler(exc, context):
+    response = exception_handler(exc, context)
+    if response is not None:
+        detail = exc.get_full_details()
+        status_code = detail.get('status',{}).get('message') or response.status_code
+        messages = [m['message'] for m in detail.get('messages',{})]
+        if not messages:
+            messages = [detail.get('message')]
+        if not any(messages):
+            messages = []
+        response.data = {
+            'result':{},
+            'messages':messages,
+            'status':status_code
+        }
+    return response
+
 
 def _execption_detail(status_code,messages=[]):
     return {
@@ -7,6 +27,11 @@ def _execption_detail(status_code,messages=[]):
         'status':status_code,
         'messages':messages
     }
+
+
+def get_messages_serializer(errors):
+    return [str(err[0]) for err in errors.values()]
+
 
 class DetailDictMixin:
     def __init__(self,messages=[],status_code=None,default_code=None):
@@ -29,7 +54,7 @@ class Exception(DetailDictMixin,APIException):
 class BadRequest(Exception):
     status_code = status.HTTP_400_BAD_REQUEST
     default_code = "bad_request"
-    default_detail = _execption_detail(status_code,['Bad request check fields'])
+    default_detail = _execption_detail(status_code,['Bad request'])
 
 
 class AuthenticationNotProvided(Exception):
@@ -48,3 +73,21 @@ class InvalidToken(Exception):
     status_code = status.HTTP_401_UNAUTHORIZED
     default_code = "token_is_not_valid"
     default_detail = _execption_detail(status_code,['Token is invalid'])
+
+
+class InvalidFormatFile(Exception):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_code = "format_file_not_valid"
+    default_detail = _execption_detail(status_code,['Format file is invalid'])
+
+
+class InvalidEmail(Exception):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_code = "email_not_valid"
+    default_detail = _execption_detail(status_code,['Email is invalid'])
+
+
+class InvalidField(Exception):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_code = "field_not_valid"
+    default_detail = _execption_detail(status_code,['Field is invalid'])
