c29daf7134edb58f875a7ad9a87ab6b1def8ea70
Fazel Momeni <wwwfazel88@gmail.com>
2023-03-04 06:19:37 +0330

    Add email configuration

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG',True)
+# Cant use bool in .env
+DEBUG = os.environ.get('DEBUG',"True") == "True"
 
-ALLOWED_HOSTS = json.loads(os.environ.get('ALLOWED_HOSTS',[]))
+
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -125,7 +127,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -189,3 +191,12 @@ REDIS_CONFIG = {
     'host': 'localhost',
     'port': '6379'
 }
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
