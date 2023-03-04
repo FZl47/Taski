46356d461c025e39731b0d03c33f81be63afbc55
Fazel Momeni <wwwfazel88@gmail.com>
2023-03-04 07:27:44 +0330

    and slash in url static and media files

--- config/settings.py
+++ config/settings.py
@@ -32,6 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG',"True") == "True"
 
 
+
 ALLOWED_HOSTS = []
 
 
@@ -133,11 +134,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
