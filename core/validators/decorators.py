e32c315da14279e786465900eb8c60484875fea9
Fazel Momeni <wwwfazel88@gmail.com>
2023-03-28 04:12:05 +0430

    Add validators pack

---
+++ core/validators/decorators.py
@@ -0,0 +1,32 @@
+from django.conf import settings as _settings
+from core import exceptions
+
+
+def validator_image_format(func):
+    """
+        This decorator should use for path function
+        like : ImageField(upload_to= this_func )
+    """
+
+    def wrapper(instance, path, *args, **kwargs):
+        path_format = str(path).split('.')[-1]
+        if path_format not in _settings.IMAGES_FORMAT:
+            raise exceptions.InvalidFormatImage
+        return func(instance, path, *args, **kwargs)
+
+    return wrapper
+
+
+def validator_file_format(func):
+    """
+        This decorator should use for path function
+        like : FileField(upload_to= this_func )
+    """
+
+    def wrapper(instance, path, *args, **kwargs):
+        path_format = str(path).split('.')[-1]
+        if path_format not in _settings.FILES_FORMAT:
+            raise exceptions.InvalidFormatFile
+        return func(instance, path, *args, **kwargs)
+    return wrapper
+
