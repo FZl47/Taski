39dcd8cdcd8cd6eb7031c9c83ec1f5fd295a6533
Fazel Momeni <wwwfazel88@gmail.com>
2023-03-30 06:33:22 +0430

    add extra message for IsOwnerGroup permission

--- task/permissions.py
+++ task/permissions.py
@@ -8,7 +8,8 @@ class IsOwnerGroup(BasePermission):
         group_id = view.kwargs.get('group_id')
         try:
             group = models.Group.objects.get(id=group_id,owner=request.user)
+            # set group attr for use later in view
             setattr(request,'group',group)
         except:
-            raise exceptions.PermissionDenied(['Only owner group can access'])
+            raise exceptions.PermissionDenied(['Only group owner can access or Group Not found with this ID'])
         return True
