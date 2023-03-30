c199d61626572de59492b0bdfd08444bc18a261b
Fazel Momeni <wwwfazel88@gmail.com>
2023-03-30 06:34:20 +0430

    create BaseModelMixin for all model with uuid attr

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class BaseModelMixin(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True,primary_key=True)
+
+    class Meta:
+        abstract = True
