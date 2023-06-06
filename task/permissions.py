5e3a2d0801a65dda319b9d942756dbe67e90298f
Fazel Momeni <wwwfazel88@gmail.com>
2023-06-06 23:39:50 +0330

    fix permission file

--- task/permissions.py
+++ task/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import BasePermission
+from core.models import get_object_or_none
 from core import exceptions
 from . import models
 
@@ -6,21 +7,27 @@ from . import models
 class IsOwnerGroup(BasePermission):
     def has_permission(self, request, view):
         group_id = view.kwargs.get('group_id')
-        try:
-            group = models.Group.objects.get(id=group_id,owner=request.user)
-            # set group attr for use later in view
+        group = get_object_or_none(models.Group,id=group_id, owner=request.user)
+        if group:
+            # set group attr for use in view
             setattr(request,'group',group)
-        except:
+            setattr(request,'is_owner_group',True) # is Owner
+        else:
             raise exceptions.PermissionDenied(['Only group owner can access or Group Not found with this ID'])
         return True
+
 
 class IsOwnerOrAdminGroup(BasePermission):
     def has_permission(self, request, view):
         group_id = view.kwargs.get('group_id')
-        try:
-            group = models.Group.objects.get(id=group_id,owner=request.user)
-            # set group attr for use later in view
-            setattr(request,'group',group)
-        except:
+        group = get_object_or_none(models.Group, id=group_id, owner=request.user)
+        if group:
+            setattr(request, 'is_owner_group', True)  # is Owner
+        else:
+            group = get_object_or_none(models.Group, id=group_id, admins__group=request.user)
+            setattr(request, 'is_owner_group', False)  # is Admin
+        # set group attr for use in view
+        if group is None:
             raise exceptions.PermissionDenied(['Only group owner or admins can access or Group Not found with this ID'])
+        setattr(request, 'group', group)
         return True
