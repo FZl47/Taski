55aa0bbaa7b9e0bcc715e8bbbc7cdc21199e857e
Fazel Momeni <wwwfazel88@gmail.com>
2023-06-23 02:39:43 +0330

    add some point

--- task/urls.py
+++ task/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'task'
 urlpatterns = [
-
+    path('task/<uuid:group_id>/all', views.TaskList.as_view(), name='task_list'),
+    path('task/<uuid:group_id>/create', views.CreateTask.as_view(), name='create_task'),
+    path('task/file/<uuid:group_id>/create', views.CreateTaskFile.as_view(), name='create_task_file'),
 ]
