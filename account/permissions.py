3d0f92aa94eafd724a04d551767822fdaa4e85cc
Fazel Momeni <wwwfazel88@gmail.com>
2023-06-23 02:36:34 +0330

    change structure permissions

--- account/permissions.py
+++ account/permissions.py
@@ -6,28 +6,31 @@ from . import models
 
 class IsOwnerGroup(BasePermission):
     def has_permission(self, request, view):
-        group_id = view.kwargs.get('group_id')
-        group = get_object_or_none(models.Group,id=group_id, owner=request.user)
-        if group:
-            # set group attr for use in view
-            setattr(request,'group',group)
-            setattr(request,'is_owner_group',True) # is Owner
-        else:
-            raise exceptions.PermissionDenied(['Only group owner can access or Group Not found with this ID'])
+        group_id = view.kwargs.get('group_id') or request.data.get('group_id')
+        group = get_object_or_none(models.Group,id=group_id)
+        owner = get_object_or_none(models.GroupAdmin,is_owner=True,user=request.user,group=group)
+        if group is None:
+            raise exceptions.NotFound(['Group Not found with this ID'])
+        if owner is None:
+            raise exceptions.PermissionDenied(['Only group owner can access.'])
+        # set group attr for use in view
+        setattr(request, 'group', group)
+        setattr(request, 'admin', owner)  # group admin object
+        setattr(request, 'is_owner_group', True)  # is Owner
         return True
 
 
 class IsOwnerOrAdminGroup(BasePermission):
     def has_permission(self, request, view):
-        group_id = view.kwargs.get('group_id')
-        group = get_object_or_none(models.Group, id=group_id, owner=request.user)
-        if group:
-            setattr(request, 'is_owner_group', True)  # is Owner
-        else:
-            group = get_object_or_none(models.Group, id=group_id, admins__group=request.user)
-            setattr(request, 'is_owner_group', False)  # is Admin
-        # set group attr for use in view
+        group_id = view.kwargs.get('group_id') or request.data.get('group_id')
+        group = get_object_or_none(models.Group, id=group_id)
+        admin = get_object_or_none(models.GroupAdmin,user=request.user,group=group)
         if group is None:
-            raise exceptions.PermissionDenied(['Only group owner or admins can access or Group Not found with this ID'])
+            raise exceptions.NotFound(['Group Not found with this ID'])
+        if admin is None:
+            raise exceptions.PermissionDenied(['Only group owner or admins can access.'])
+        # set group attr for use in view
         setattr(request, 'group', group)
+        setattr(request, 'admin', admin)
+        setattr(request, 'is_owner_group', admin.is_owner)  # Admin or Owner
         return True
