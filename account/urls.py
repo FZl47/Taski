f8da5c4f9cae1c45fd45d859c6a20f0aa680c46a
Fazel Momeni <wwwfazel88@gmail.com>
2023-06-23 02:39:29 +0330

    some refactor name - add some path

--- account/urls.py
+++ account/urls.py
@@ -6,13 +6,14 @@ from . import views
 app_name = 'account'
 urlpatterns = [
     # User
-    path('register/',views.Register.as_view(),name='register'),
-    path('login/',views.Login.as_view(),name='login'),
-    path('update-user/',views.UserUpdate.as_view(),name='update_user'),
-    path('reset-password/',views.ResetPassword.as_view(),name='reset_password'),
-    path('reset-password-code/',views.ResetPasswordCode.as_view(),name='reset_password_code'),
-    path('delete-user/',views.UserDelete.as_view(),name='delete_user'),
-    path('token/refresh/',views.AccessToken.as_view(),name='access_token'),
+    path('user/register',views.Register.as_view(),name='register'),
+    path('user/login',views.Login.as_view(),name='login'),
+    path('user/update',views.UserUpdate.as_view(),name='update_user'),
+    path('user/reset-password',views.ResetPassword.as_view(),name='reset_password'),
+    path('user/reset-password-code',views.ResetPasswordCode.as_view(),name='reset_password_code'),
+    path('user/delete',views.UserDelete.as_view(),name='delete_user'),
+    path('user/token/refresh',views.AccessToken.as_view(),name='access_token'),
+
 
     # Group
     path('group/create',views.CreateGroup.as_view(),name='create_group'),
@@ -23,10 +24,11 @@ urlpatterns = [
     path('group/<uuid:group_id>/users/add/request',views.RequestAddUserToGroup.as_view(),name='request_add_user_group'),
     path('group/request/accept/<uuid:token>',views.AcceptRequestGroupJoin.as_view(),name='accept_request_group_join'),
     path('group/<uuid:group_id>/users/<uuid:user_id>/delete',views.DeleteGroupUser.as_view(),name='group_user_delete'),
+    path('group/all',views.GroupList.as_view(),name='group_list'),
 
     # Admin
-    path('group/admin/create',views.CreateAdminGroup.as_view(),name='create_admin_group'),
-    path('group/<uuid:group_id>/admins/add',views.AddAdminToGroup.as_view(),name='add_admin_to_group'),
+    path('group/<uuid:group_id>/admins/create',views.CreateAdminGroup.as_view(),name='create_admin_group'),
+    # path('group/<uuid:group_id>/admins/add',views.AddAdminToGroup.as_view(),name='add_admin_to_group'),
     path('group/<uuid:group_id>/admins/<uuid:admin_id>/delete',views.DeleteGroupAdmin.as_view(),name='group_admin_delete'),
     path('group/<uuid:group_id>/admins',views.GroupAdmins.as_view(),name='group_admins'),
 ]
