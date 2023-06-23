821c23072a73ec4fe510ae0865a07451e79e96ea
Fazel Momeni <wwwfazel88@gmail.com>
2023-06-23 23:57:53 +0330

    change some structure Task and TaskFile relation

--- task/models.py
+++ task/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.crypto import get_random_string
+from django.conf import settings
 from core import validators
 from core.models import BaseModelMixin
 from core.mixins.model.delete_file import RemovePastFileMixin
@@ -10,16 +11,16 @@ def upload_src_task_file(instance,path):
     """
         return src task file in media
     """
-    instance_id = instance.pk or get_random_string(13)
-    return f"files/task/{instance_id}/{get_random_string(10)}.{path}"
+    path = str(path).split('.')[-1]
+    return f"files/group/{instance.task.group.id}/task/{instance.task.id}/{get_random_string(10)}.{path}"
 
 
 class Task(BaseModelMixin,models.Model):
+    create_by = models.ForeignKey('account.GroupAdmin',on_delete=models.CASCADE) # TODO : should be complete view and tests
     group = models.ForeignKey('account.Group',on_delete=models.CASCADE)
     users = models.ManyToManyField('account.User')
     title = models.CharField(max_length=200)
     description = models.TextField(null=True,blank=True)
-    attach = models.ManyToManyField('TaskFile',null=True,blank=True)
     datetime_create = models.DateTimeField(auto_now_add=True)
     datetime_update = models.DateTimeField(auto_now=True)
     timeleft = models.DateTimeField(null=True,blank=True)
@@ -30,11 +31,15 @@ class Task(BaseModelMixin,models.Model):
 
 class TaskFile(BaseModelMixin,RemovePastFileMixin,models.Model):
     FIELDS_REMOVE_FILES = ['image']
-
-    file = models.FileField(upload_to=upload_src_task_file)
+    task = models.ForeignKey('Task',on_delete=models.CASCADE)
+    file = models.FileField(upload_to=upload_src_task_file,max_length=300)
     datetime_create = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"ATTACH FILE #{self.id} - {self.task.title}"
 
+    def get_file(self):
+        return settings.GET_FULL_HOST(self.file.url)
 
 
 
