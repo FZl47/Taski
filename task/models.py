43883b641908b885074ac5670cc16d3d745a8da5
Fazel Momeni <wwwfazel88@gmail.com>
2023-06-24 05:19:51 +0330

    add field 'label' add validator datetime on 'timeleft' field

--- task/models.py
+++ task/models.py
@@ -1,6 +1,8 @@
+import datetime
 from django.db import models
 from django.utils.crypto import get_random_string
 from django.conf import settings
+from django.core.validators import MinValueValidator
 from core import validators
 from core.models import BaseModelMixin
 from core.mixins.model.delete_file import RemovePastFileMixin
@@ -20,13 +22,17 @@ class Task(BaseModelMixin,models.Model):
     group = models.ForeignKey('account.Group',on_delete=models.CASCADE)
     users = models.ManyToManyField('account.User')
     title = models.CharField(max_length=200)
+    label = models.CharField(max_length=100,null=True,blank=True)
     description = models.TextField(null=True,blank=True)
     datetime_create = models.DateTimeField(auto_now_add=True)
     datetime_update = models.DateTimeField(auto_now=True)
-    timeleft = models.DateTimeField(null=True,blank=True)
+    timeleft = models.DateTimeField(null=True,blank=True,validators=[MinValueValidator(datetime.datetime.now)])
 
     def __str__(self):
         return f"#{self.group_id} - {self.title[:30]}"
+
+    def get_users_id(self):
+        return list(self.users.all().values_list('id',flat=True))
 
 
 class TaskFile(BaseModelMixin,RemovePastFileMixin,models.Model):
