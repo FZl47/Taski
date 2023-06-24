cea8f580167a39bb7c4afc52815ee0e20c67a22f
Fazel Momeni <wwwfazel88@gmail.com>
2023-06-24 05:20:04 +0330

    add some points

--- task/urls.py
+++ task/urls.py
@@ -5,5 +5,8 @@ app_name = 'task'
 urlpatterns = [
     path('task/<uuid:group_id>/all', views.TaskList.as_view(), name='task_list'),
     path('task/<uuid:group_id>/create', views.CreateTask.as_view(), name='create_task'),
+    path('task/<uuid:group_id>/<uuid:task_id>/update', views.UpdateTask.as_view(), name='update_task'),
+    path('task/<uuid:group_id>/<uuid:task_id>/users/update', views.UpdateUsersTask.as_view(), name='update_users_task'),
+    path('task/<uuid:group_id>/<uuid:task_id>/delete', views.DeleteTask.as_view(), name='delete_task'),
     path('task/file/<uuid:group_id>/create', views.CreateTaskFile.as_view(), name='create_task_file'),
 ]
