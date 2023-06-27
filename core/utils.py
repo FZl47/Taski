81e53a917a41ff9010d04dd68a7b74c633c87d08
Fazel Momeni <wwwfazel88@gmail.com>
2023-06-28 02:02:09 +0330

    use django-q async_task instead of task_bg

--- core/utils.py
+++ core/utils.py
@@ -2,9 +2,9 @@ import datetime
 import random
 from django.core.mail import send_mail as _send_email_django, EmailMultiAlternatives
 from django.conf import settings
-from core.task_bg import Thread, Task
+from django_q.tasks import async_task
 
-_Thread = Thread()
+
 
 def get_datetime():
     """
@@ -12,24 +12,34 @@ def get_datetime():
     """
     return datetime.datetime.now()
 
-def get_datetime_f():
+
+def get_datetime_f(f='%Y-%m-%d %H:%M:%S'):
     """
         return datetime with custom format time
     """
-    return get_datetime().strftime('%Y-%m-%d %H:%M:%S')
+    return get_datetime().strftime(f)
 
 
-def send_email(subject:str,message:str,email_receiver:list):
+def get_datetime_div(dt, div):
+    """
+        return division on datetime
+    """
+    tm = datetime.timedelta(days=dt.day, minutes=dt.minute, seconds=dt.second)
+    tm = tm / div
+    assert div <= 100
+    dt = dt - tm
+    return dt
+
+
+def send_email(subject: str, message: str, email_receiver: list):
     """
         send email in background
     """
     email_from = settings.EMAIL_HOST_USER
-    t = Task(_send_email_django,args=(subject, message, email_from, email_receiver))
-    _Thread.add(t)
-    _Thread.start_or_continue()
+    async_task(_send_email_django,(subject, message, email_from, email_receiver))
 
 
-def send_email_html(subject:str,message:str,email_receiver:list):
+def send_email_html(subject: str, message: str, email_receiver: list):
     email_from = settings.EMAIL_HOST_USER
     email = EmailMultiAlternatives(
         subject=subject,
@@ -37,9 +47,7 @@ def send_email_html(subject:str,message:str,email_receiver:list):
         to=email_receiver
     )
     email.attach_alternative(message, "text/html")
-    t = Task(email.send)
-    _Thread.add(t)
-    _Thread.start_or_continue()
+    async_task(email.send)
 
 
 def random_num(n=6):
