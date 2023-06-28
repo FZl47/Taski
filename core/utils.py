3cb9d1f7f77e52ab315d7b34867dbc517ecbfd5e
Fazel Momeni <wwwfazel88@gmail.com>
2023-06-28 06:55:02 +0330

    add some func

--- core/utils.py
+++ core/utils.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django_q.tasks import async_task
 
 
-
 def get_datetime():
     """
         return datetime
@@ -20,15 +19,22 @@ def get_datetime_f(f='%Y-%m-%d %H:%M:%S'):
     return get_datetime().strftime(f)
 
 
-def get_datetime_div(dt, div):
+def get_datetime_div(dt_start, dt_end, div):
     """
         return division on datetime
     """
-    tm = datetime.timedelta(days=dt.day, minutes=dt.minute, seconds=dt.second)
-    tm = tm / div
     assert div <= 100
-    dt = dt - tm
-    return dt
+    dt = dt_end - dt_start
+    tm = dt / div
+    res = dt_end - tm
+    return res
+
+
+def get_days_hours_minutes_td(td) -> tuple:
+    """
+        return days hours and minutes from timedelta object
+    """
+    return td.days, td.seconds // 3600, (td.seconds // 60) % 60
 
 
 def send_email(subject: str, message: str, email_receiver: list):
@@ -36,7 +42,7 @@ def send_email(subject: str, message: str, email_receiver: list):
         send email in background
     """
     email_from = settings.EMAIL_HOST_USER
-    async_task(_send_email_django,(subject, message, email_from, email_receiver))
+    async_task(_send_email_django, (subject, message, email_from, email_receiver))
 
 
 def send_email_html(subject: str, message: str, email_receiver: list):
