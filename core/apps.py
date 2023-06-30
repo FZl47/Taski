4c2d7a2d0ee76e2b33b61dde31011cb6e8daef2f
Fazel Momeni <wwwfazel88@gmail.com>
2023-06-30 04:04:59 +0330

    change init tasks from core to public app

--- core/apps.py
+++ core/apps.py
@@ -5,14 +5,4 @@ class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
 
-    def ready(self):
-        # init tasks
 
-        # set crontab for clean Schedule tasks
-        from django_q.models import Schedule
-        Schedule.objects.create(
-            name='task_clean_schedule_tasks',
-            func='core.tasks.delete_old_schedule_task',
-            schedule_type='W', # Weekly period
-            repeats=-1 # run forever
-        )
