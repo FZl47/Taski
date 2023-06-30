cfd59fc7a6f288105f9a7f1fae8b9e395e4673ef
Fazel Momeni <wwwfazel88@gmail.com>
2023-06-30 04:05:19 +0330

    add two default field in BaseModelMixin

--- core/models.py
+++ core/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class BaseModelMixin(models.Model):
     id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True,primary_key=True)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
