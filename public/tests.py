98f6fe85b67984b7fa780782a0ce23c905e404d3
Fazel Momeni <wwwfazel88@gmail.com>
2023-06-30 04:06:57 +0330

    add test for external service check

--- public/tests.py
+++ public/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.conf import settings
+from core import redis_py
 
-# Create your tests here.
+
+class TestThirdPartyService(TestCase):
+
+    def test_redis(self):
+        r = redis_py.test_conn()
+        self.assertEqual(r, True, 'Redis service is not running')
