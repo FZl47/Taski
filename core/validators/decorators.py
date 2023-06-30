8b0373b600b42a87398e806942e2257adfe1d3cc
Fazel Momeni <wwwfazel88@gmail.com>
2023-06-30 04:06:10 +0330

    style

--- core/validators/decorators.py
+++ core/validators/decorators.py
@@ -3,12 +3,12 @@ from django.conf import settings as _settings
 from core import exceptions
 
 
-
 def validator_image_format(func):
     """
         This decorator should use for path function
         like : ImageField(upload_to= this_func )
     """
+
     @wraps(validator_image_format)
     def wrapper(instance, path, *args, **kwargs):
         path_format = str(path).split('.')[-1]
@@ -24,11 +24,12 @@ def validator_file_format(func):
         This decorator should use for path function
         like : FileField(upload_to= this_func )
     """
+
     @wraps(validator_file_format)
     def wrapper(instance, path, *args, **kwargs):
         path_format = str(path).split('.')[-1]
         if path_format not in _settings.FILES_FORMAT:
             raise exceptions.InvalidFormatFile
         return func(instance, path, *args, **kwargs)
-    return wrapper
 
+    return wrapper
