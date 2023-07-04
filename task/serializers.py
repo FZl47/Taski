5779810722d2221864b9ebc7f3b59d833b410790
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-04 13:04:28 +0330

    fix serializers and use BaseSerializer | new structure

--- task/serializers.py
+++ task/serializers.py
@@ -1,180 +1,193 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
+from core.serializers import BaseSerializer
 from . import models
 
 
-class TaskListViewParameterSerializer(serializers.Serializer):
-    SORT_BY_OPTIONS = (
-        ('latest', 'default'),
-        ('oldest', 'Oldest'),
-        ('expire_date_desc', 'later expiration date'),
-        ('expire_date_asc', 'closer expiration date'),
-    )
-    FILTER_BY_COMPLETE_STATE = (
-        ('all', 'default'),
-        ('true', 'filter tasks where taks is completed'),
-        ('false', 'filter tasks where taks is not completed'),
-    )
-    sort_by = serializers.ChoiceField(SORT_BY_OPTIONS, default=SORT_BY_OPTIONS[0][0])
-    is_completed = serializers.ChoiceField(FILTER_BY_COMPLETE_STATE,
-                                           default=FILTER_BY_COMPLETE_STATE[0][0],
-                                           help_text="filter tasks by complete state"
-                                           )
+class TaskResponseFile:
+    # CREATE view
+    class Create(BaseSerializer, ModelSerializer):
+        class Meta:
+            model = models.TaskResponseFile
+            fields = ('id', 'file', 'task_response', 'datetime_created', 'datetime_updated')
+
+    # GET view
+    class Get(BaseSerializer, ModelSerializer):
+        file = serializers.URLField(source='get_file')
+
+        class Meta:
+            model = models.TaskResponseFile
+            fields = ('file', 'datetime_created', 'datetime_updated')
 
 
-class CreateTaskFileAttachSerializer(ModelSerializer):
-    class Meta:
-        model = models.TaskFile
-        fields = ('file', 'task')
+class TaskFile:
+    # CREATE view
+    class CreateRequestBody(BaseSerializer, ModelSerializer):
+        class Meta:
+            model = models.TaskFile
+            fields = ('file', 'task')
+
+    class Create(BaseSerializer, ModelSerializer):
+        file = serializers.URLField(source='get_file')
+
+        class Meta:
+            model = models.TaskFile
+            fields = ('id', 'file', 'task', 'datetime_created', 'datetime_updated')
+
+    # UPDATE view
+    class UpdateRequestBody(BaseSerializer, ModelSerializer):
+        class Meta:
+            model = models.TaskFile
+            fields = ('task', 'file')
+
+    class Update(BaseSerializer, ModelSerializer):
+        file = serializers.URLField(source='get_file')
+
+        class Meta:
+            model = models.TaskFile
+            fields = ('id', 'file', 'datetime_updated')
+
+    # GET view
+    class Get(BaseSerializer, ModelSerializer):
+        file = serializers.URLField(source='get_file')
+
+        class Meta:
+            model = models.TaskFile
+            fields = ('id', 'file', 'task', 'datetime_created', 'datetime_updated')
+
+    # DELETE view
+    class DeleteRequestBody(BaseSerializer, ModelSerializer):
+        class Meta:
+            model = models.TaskFile
+            fields = ('task',)
+
+    class Delete(BaseSerializer, ModelSerializer):
+        file = serializers.URLField(source='get_file')
+
+        class Meta:
+            model = models.TaskFile
+            fields = '__all__'
 
 
-class CreateTaskFileAttachResponseSerializer(ModelSerializer):
-    file = serializers.URLField(source='get_file')
+class TaskResponse:
+    # CREATE view
+    class CreateRequestBody(BaseSerializer, ModelSerializer):
+        class Meta:
+            model = models.TaskResponse
+            fields = ('task', 'content')
 
-    class Meta:
-        model = models.TaskFile
-        fields = ('id', 'file', 'task', 'datetime_created', 'datetime_updated')
+    class Create(BaseSerializer, ModelSerializer):
+        class Meta:
+            model = models.TaskResponse
+            fields = '__all__'
 
+    # UPDATE view
+    class UpdateRequestBody(BaseSerializer, ModelSerializer):
+        class Meta:
+            model = models.TaskResponse
+            fields = ('content', 'task')
 
-class GetTaskFileAttachSerializer(ModelSerializer):
-    file = serializers.URLField(source='get_file')
+    class Update(BaseSerializer, ModelSerializer):
+        class Meta:
+            model = models.TaskResponse
+            fields = '__all__'
 
-    class Meta:
-        model = models.TaskFile
-        fields = ('id', 'file', 'datetime_created', 'datetime_updated')
+    # GET view
+    class Get(BaseSerializer, ModelSerializer):
+        attach = TaskResponseFile.Get(many=True, source='taskresponsefile_set', read_only=True)
 
+        class Meta:
+            model = models.TaskResponse
+            fields = '__all__'
 
-class UpdateTaskFileAttachSerializer(ModelSerializer):
-    class Meta:
-        model = models.TaskFile
-        fields = ('file',)
+    # DELETE view
+    class DeleteRequestBody(BaseSerializer, ModelSerializer):
+        class Meta:
+            model = models.TaskResponse
+            fields = ('task',)
 
+    class Delete(BaseSerializer, ModelSerializer):
+        attach_count = serializers.SerializerMethodField()
 
-class UpdateTaskFileAttachResponseSerializer(ModelSerializer):
-    file = serializers.URLField(source='get_file')
+        class Meta:
+            model = models.TaskResponse
+            exclude = ('id', 'datetime_created', 'datetime_updated')
 
-    class Meta:
-        model = models.TaskFile
-        fields = ('id', 'file', 'datetime_updated')
-
-
-class DeleteTaskFileAttachSerializer(ModelSerializer):
-    class Meta:
-        model = models.TaskFile
-        fields = ('task',)
-
-
-class CreateTaskRequestBodySerializer(ModelSerializer):
-    """
-        Just for use in swagger
-    """
-
-    class Meta:
-        model = models.Task
-        exclude = ('is_active', 'group', 'created_by')
-
-
-class CreateTaskSerializer(ModelSerializer):
-    class Meta:
-        model = models.Task
-        exclude = ('is_active',)
+        def get_attach_count(self, obj):
+            return obj.taskresponsefile_set.count()
 
 
-class TaskSerializer(ModelSerializer):
-    attach = GetTaskFileAttachSerializer(many=True, source='taskfile_set', read_only=True)
-    expired = serializers.BooleanField(source='is_expired', read_only=True)
-    time_late = serializers.CharField(source='get_time_late', read_only=True)
+class Task:
+    # CREATE view
+    class CreateSwagger(BaseSerializer, ModelSerializer):
+        class Meta:
+            model = models.Task
+            exclude = ('is_active', 'is_completed', 'group', 'created_by')
 
-    class Meta:
-        model = models.Task
-        exclude = ('group', 'user')
+    class CreateRequestBody(BaseSerializer, ModelSerializer):
+        class Meta:
+            model = models.Task
+            exclude = ('is_active',)
 
+    class Create(CreateRequestBody):
+        # just for response swagger
+        pass
 
-class DeleteTaskSerializer(ModelSerializer):
-    class Meta:
-        model = models.Task
-        fields = ('id', 'group')
-
-
-class UpdateTaskSerializer(ModelSerializer):
-    class Meta:
-        model = models.Task
-        fields = ('title', 'description', 'label', 'timeleft', 'is_completed', 'datetime_created', 'datetime_updated')
-        extra_kwargs = {
-            'title': {
-                'required': False
-            },
-            'datetime_created': {
-                'read_only': True
-            },
-            'datetime_updated': {
-                'read_only': True
+    # UPDATE view
+    class UpdateRequestBody(BaseSerializer, ModelSerializer):
+        class Meta:
+            model = models.Task
+            fields = (
+                'title', 'description', 'label', 'timeleft', 'is_completed', 'datetime_created', 'datetime_updated')
+            extra_kwargs = {
+                'title': {
+                    'required': False
+                },
+                'datetime_created': {
+                    'read_only': True
+                },
+                'datetime_updated': {
+                    'read_only': True
+                }
             }
-        }
 
+    class Update(UpdateRequestBody):
+        pass
 
-class CreateTaskResponseRequestBodySerializer(ModelSerializer):
-    class Meta:
-        model = models.TaskResponse
-        fields = ('content',)
+    # GET view
+    class ListRequestParameter(serializers.Serializer):
+        SORT_BY_OPTIONS = (
+            ('latest', 'default'),
+            ('oldest', 'Oldest'),
+            ('expire_date_desc', 'later expiration date'),
+            ('expire_date_asc', 'closer expiration date'),
+        )
+        FILTER_BY_COMPLETE_STATE = (
+            ('all', 'default'),
+            ('true', 'filter tasks where taks is completed'),
+            ('false', 'filter tasks where taks is not completed'),
+        )
+        sort_by = serializers.ChoiceField(SORT_BY_OPTIONS, default=SORT_BY_OPTIONS[0][0])
+        is_completed = serializers.ChoiceField(FILTER_BY_COMPLETE_STATE,
+                                               default=FILTER_BY_COMPLETE_STATE[0][0],
+                                               help_text="filter tasks by complete state")
 
+    class Get(ModelSerializer):
+        attach = TaskFile.Get(many=True, source='taskfile_set', read_only=True)
+        expired = serializers.BooleanField(source='is_expired', read_only=True)
+        time_late = serializers.CharField(source='get_time_late', read_only=True)
 
-class CreateTaskResponseSerializer(ModelSerializer):
-    class Meta:
-        model = models.TaskResponse
-        fields = '__all__'
+        class Meta:
+            model = models.Task
+            exclude = ('group',)
 
+    # DELETE view
+    class DeleteRequestBody(BaseSerializer, ModelSerializer):
+        class Meta:
+            model = models.Task
+            fields = ('group',)
 
-class CreateTaskResponseResponseSerializer(ModelSerializer):
-    class Meta:
-        model = models.TaskResponse
-        exclude = ('user',)
-
-
-class UpdateTaskResponseSerializer(ModelSerializer):
-    class Meta:
-        model = models.TaskResponse
-        fields = ('content', 'user', 'task')
-        extra_kwargs = {
-            'user': {
-                'read_only': True
-            },
-            'task': {
-                'read_only': True
-            }
-        }
-
-
-class TaskResponseFileSerializer(ModelSerializer):
-    file = serializers.URLField(source='get_file')
-
-    class Meta:
-        model = models.TaskResponseFile
-        fields = ('file', 'datetime_created', 'datetime_updated')
-
-
-class GetTaskResponseSerializer(ModelSerializer):
-    attach = TaskResponseFileSerializer(many=True, source=' taskfileresponse_set', read_only=True)
-
-    class Meta:
-        model = models.TaskResponse
-        exclude = ('user',)
-
-
-class DeleteTaskResponseSerializer(ModelSerializer):
-    attach_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = models.TaskResponse
-        exclude = ('id', 'user', 'datetime_created', 'datetime_updated')
-
-    def get_attach_count(self, obj):
-        return obj.taskfileresponse_set.count()
-#
-#
-# class CreateTaskResponseFileResponseSerializer(ModelSerializer):
-#     file = serializers.URLField(source='get_file')
-#
-#     class Meta:
-#         model = models.TaskFileResponse
-#         fields = '__all__'
+    class Delete(BaseSerializer, ModelSerializer):
+        class Meta:
+            model = models.Task
+            fields = ('id', 'group')
