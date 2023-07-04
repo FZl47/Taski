15432b506a68021ecfcd388be53a823fc5edb818
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-04 13:06:42 +0330

    use new structure swagger

--- task/swagger.py
+++ task/swagger.py
@@ -1,39 +1,171 @@
 from . import serializers
 
+Task = {
+    'tags': ['Task'],
+    'views': {
+        'Create': {
+            'methods': {
+                'post': {
+                    'title': 'Create Task',
+                    'description': 'create task for user',
+                    'request_body': serializers.Task.CreateSwagger,
+                    'responses': {
+                        200: serializers.Task.Create
+                    }
+                },
+            }
+        },
+        'Update': {
+            'methods': {
+                'put': {
+                    'title': 'Update Task',
+                    'description': 'update task',
+                    'request_body': serializers.Task.UpdateRequestBody,
+                    'responses': {
+                        200: serializers.Task.Update
+                    }
+                },
+            }
+        },
+        'List': {
+            'methods': {
+                'get': {
+                    'title': 'Get Tasks',
+                    'description': 'get user tasks',
+                    'query_serializer': serializers.Task.ListRequestParameter,
+                    'responses': {
+                        200: serializers.Task.Get(many=True)
+                    },
+                },
+            }
+        },
+        'Retrieve': {
+            'methods': {
+                'get': {
+                    'title': 'Get Task Detail',
+                    'description': 'get task detail',
+                    'responses': {
+                        200: serializers.Task.Get
+                    },
+                },
+            }
+        },
+        'Delete': {
+            'methods': {
+                'delete': {
+                    'title': 'Delete Task',
+                    'description': 'delete task',
+                    'responses': {
+                        200: serializers.Task.Delete
+                    },
+                }
+            }
+        }
+    }
+}
+
+TaskFile = {
+    'tags': ['Task'],
+    'views': {
+        'Create': {
+            'methods': {
+                'post': {
+                    'title': 'Create Task File',
+                    'description': 'create file attach',
+                    'request_body': serializers.TaskFile.CreateRequestBody,
+                    'responses': {
+                        200: serializers.TaskFile.Create
+                    }
+                },
+            }
+        },
+        'Update': {
+            'methods': {
+                'put': {
+                    'title': 'Update Task File',
+                    'description': 'update file attach',
+                    'request_body': serializers.TaskFile.UpdateRequestBody,
+                    'responses': {
+                        200: serializers.TaskFile.Update
+                    }
+                },
+            }
+        },
+        'Retrieve': {
+            'methods': {
+                'get': {
+                    'title': 'Get Task File',
+                    'description': 'get file attach',
+                    'responses': {
+                        200: serializers.TaskFile.Get
+                    }
+                },
+            }
+        },
+        'Delete': {
+            'methods': {
+                'delete': {
+                    'title': 'Delete Task File',
+                    'description': 'delete file attach',
+                    'request_body': serializers.TaskFile.DeleteRequestBody,
+                    'responses': {
+                        200: serializers.TaskFile.Delete
+                    }
+                },
+            }
+        }
+    },
+}
 
 TaskResponse = {
     'tags': ['Task'],
-    'methods': {
-        'post': {
-            'title': 'Create Task Response',
-            'description': 'create task response',
-            'request_body': serializers.TaskResponse.CreateRequestBody,
-            'responses': {
-                200: serializers.TaskResponse.Create
-            },
+    'views': {
+        'Create': {
+            'methods': {
+                'post': {
+                    'title': 'Create Task Response',
+                    'description': 'create task response',
+                    'request_body': serializers.TaskResponse.CreateRequestBody,
+                    'responses': {
+                        200: serializers.TaskResponse.Create
+                    }
+                },
+            }
         },
-        'put': {
-            'title': 'Update Task Response',
-            'description': 'update task response',
-            'request_body': serializers.TaskResponse.UpdateRequestBody,
-            'responses': {
-                200: serializers.TaskResponse.Update
-            },
+        'Update': {
+            'methods': {
+                'put': {
+                    'title': 'Update Task Response',
+                    'description': 'update task response',
+                    'request_body': serializers.TaskResponse.UpdateRequestBody,
+                    'responses': {
+                        200: serializers.TaskResponse.Update
+                    }
+                },
+            }
         },
-        'get': {
-            'title': 'Get Task Response',
-            'description': 'get task response',
-            'responses': {
-                200: serializers.TaskResponse.Get
-            },
+        'Retrieve': {
+            'methods': {
+                'get': {
+                    'title': 'Get Task Response',
+                    'description': 'get task response',
+                    'responses': {
+                        200: serializers.TaskResponse.Get
+                    }
+                },
+            }
         },
-        'delete': {
-            'title': 'Delete Task Response',
-            'description': 'delete task response',
-            'request_body':serializers.TaskResponse.DeleteRequestBody,
-            'responses': {
-                200: serializers.TaskResponse.Delete
-            },
-        },
-    }
+        'Delete': {
+            'methods': {
+                'delete': {
+                    'title': 'Delete Task Response',
+                    'description': 'delete task response',
+                    'request_body': serializers.TaskResponse.DeleteRequestBody,
+                    'responses': {
+                        200: serializers.TaskResponse.Delete
+                    }
+                },
+            }
+        }
+    },
 }
