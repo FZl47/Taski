6e38164a7a7561e945806ee0fc8b8b94488279db
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-04 13:06:58 +0330

    swagger config app public

---
+++ public/swagger.py
@@ -0,0 +1,20 @@
+INDEX = {
+    'tags': ['Test Health'],
+    'methods': {
+        'post': {
+            'title': 'Test health project POST',
+            'description': '',
+            'responses': {
+                200: "",
+            },
+        },
+        'get': {
+            'title': 'Test health project GET',
+            'description': '',
+            'responses': {
+                200: "",
+            },
+        },
+
+    }
+}
