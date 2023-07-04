40e94424733e231283585273be33bccc05fc620d
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-04 12:59:39 +0330

    refactor urls task app

--- task/urls.py
+++ task/urls.py
@@ -4,20 +4,24 @@ from . import views
 app_name = 'task'
 urlpatterns = [
     # Task
-    path('task/<uuid:group_id>/all', views.TaskList.as_view(), name='task_list'),
-    path('task/<uuid:group_id>/create', views.CreateTask.as_view(), name='create_task'),
-    path('task/<uuid:group_id>/<uuid:task_id>/update', views.UpdateTask.as_view(), name='update_task'),
-    path('task/<uuid:group_id>/<uuid:task_id>/delete', views.DeleteTask.as_view(), name='delete_task'),
+    path('<uuid:group_id>/task/create', views.Task.Create.as_view(), name='task_create'),
+    path('<uuid:group_id>/task/<uuid:task_id>/update', views.Task.Update.as_view(), name='task_update'),
+    path('<uuid:group_id>/task/<uuid:task_id>', views.Task.Retrieve.as_view(), name='task_retrieve'),
+    path('<uuid:group_id>/task', views.Task.List.as_view(), name='task_list'),
+    path('<uuid:group_id>/task/<uuid:task_id>/delete', views.Task.Delete.as_view(), name='task_delete'),
+
     # Task File
-    path('task/file/<uuid:group_id>/create', views.CreateTaskFile.as_view(), name='create_task_file'),
-    path('task/file/<uuid:group_id>/<uuid:task_id>/<uuid:task_file_id>/get', views.GetTaskFile.as_view(), name='get_task_file'),
-    path('task/file/<uuid:group_id>/<uuid:task_id>/<uuid:task_file_id>/update', views.UpdateTaskFile.as_view(), name='update_task_file'),
-    path('task/file/<uuid:group_id>/<uuid:task_id>/<uuid:task_file_id>/delete', views.DeleteTaskFile.as_view(), name='delete_task_file'),
+    path('<uuid:group_id>/task/file', views.TaskFile.Create.as_view(), name='task_file_create'),
+    path('<uuid:group_id>/task/file/<uuid:task_file_id>/update', views.TaskFile.Update.as_view(), name='task_file_update'),
+    path('<uuid:group_id>/task/file/<uuid:task_file_id>', views.TaskFile.Retrieve.as_view(), name='task_file_retrieve'),
+    path('<uuid:group_id>/task/file/<uuid:task_file_id>/delete', views.TaskFile.Delete.as_view(), name='task_file_delete'),
+
     # Task Response
-    path('task/response/<uuid:group_id>/<uuid:task_id>/create', views.CreateTaskResponse.as_view(), name='create_task_response'),
-    path('task/response/<uuid:group_id>/<uuid:task_id>/<uuid:task_response_id>/update', views.UpdateTaskResponse.as_view(), name='update_task_response'),
-    path('task/response/<uuid:group_id>/<uuid:task_id>/<uuid:task_response_id>/get', views.GetTaskResponse.as_view(), name='get_task_response'),
-    path('task/response/<uuid:group_id>/<uuid:task_id>/<uuid:task_response_id>/delete', views.DeleteTaskResponse.as_view(), name='delete_task_response'),
+    path('<uuid:group_id>/task/response', views.TaskResponse.Create.as_view(), name='task_response_create'),
+    path('<uuid:group_id>/task/response/<uuid:task_response_id>/update', views.TaskResponse.Update.as_view(), name='task_response_update'),
+    path('<uuid:group_id>/task/response/<uuid:task_response_id>', views.TaskResponse.Retrieve.as_view(), name='task_response_retrieve'),
+    path('<uuid:group_id>/task/response/<uuid:task_response_id>/delete', views.TaskResponse.Delete.as_view(), name='task_response_delete'),
+
     # Task Response File
     # path('task/response/file/<uuid:group_id>/<uuid:task_id>/<uuid:task_response_id>/create', views.CreateTaskResponseFile.as_view(), name='create_task_response_file'),
 ]
