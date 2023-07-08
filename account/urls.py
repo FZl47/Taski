32e94e5f5281a498dd9c7ded44fefc7be5f1ae92
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-08 17:20:36 +0330

    fix and refactor urls

--- account/urls.py
+++ account/urls.py
@@ -6,31 +6,29 @@ from . import views
 app_name = 'account'
 urlpatterns = [
     # User
-    path('user/register',views.Register.as_view(),name='register'),
-    path('user/login',views.Login.as_view(),name='login'),
-    path('user/update',views.UserUpdate.as_view(),name='update_user'),
-    path('user/reset-password',views.ResetPassword.as_view(),name='reset_password'),
-    path('user/reset-password-code',views.ResetPasswordCode.as_view(),name='reset_password_code'),
-    path('user/delete',views.UserDelete.as_view(),name='delete_user'),
-    path('user/token/refresh',views.AccessToken.as_view(),name='access_token'),
-
+    path('user/register',views.User.Create.as_view(),name='register'),
+    path('user/login',views.User.Login.as_view(),name='login'),
+    path('user/update',views.User.UpdateUser.as_view(),name='update_user'),
+    path('user/reset-password',views.User.ResetPassword.as_view(),name='reset_password'),
+    path('user/reset-password-code',views.User.ResetPasswordCode.as_view(),name='reset_password_code'),
+    path('user/token/refresh',views.User.UpdateLogin.as_view(),name='update_login'),
+    path('user/delete',views.User.Delete.as_view(),name='delete_user'),
+    path('user/groups',views.User.GroupList.as_view(),name='groups_list_user'),
 
     # Group
-    path('group/create',views.CreateGroup.as_view(),name='create_group'),
-    path('group/<uuid:group_id>/delete',views.DeleteGroup.as_view(),name='delete_group'),
+    path('group/create',views.Group.Create.as_view(),name='create_group'),
+    path('groups/<uuid:group_id>',views.Group.Retrieve.as_view(),name='retrieve_group'),
+    path('groups/<uuid:group_id>/delete',views.Group.Delete.as_view(),name='delete_group'),
 
     # Group User
-    path('group/<uuid:group_id>/users',views.GroupUsers.as_view(),name='group_users'),
-    path('group/<uuid:group_id>/users/add/request',views.RequestAddUserToGroup.as_view(),name='request_add_user_group'),
-    path('group/request/accept/<uuid:token>',views.AcceptRequestGroupJoin.as_view(),name='accept_request_group_join'),
-    path('group/<uuid:group_id>/users/<uuid:user_id>/delete',views.DeleteGroupUser.as_view(),name='group_user_delete'),
-    path('group/all',views.GroupList.as_view(),name='group_list'),
+    path('groups/<uuid:group_id>/user/add/request',views.GroupUser.RequestAddUser.as_view(),name='request_add_group_user'),
+    path('group/request/accept/<uuid:token>',views.GroupUser.AcceptRequestJoin.as_view(),name='accept_request_join_group_user'),
+    path('groups/<uuid:group_id>/user/<uuid:user_id>/delete',views.GroupUser.Kick.as_view(),name='kick_group_user'),
 
-    # Admin
-    path('group/<uuid:group_id>/admins/create',views.CreateAdminGroup.as_view(),name='create_admin_group'),
-    # path('group/<uuid:group_id>/admins/add',views.AddAdminToGroup.as_view(),name='add_admin_to_group'),
-    path('group/<uuid:group_id>/admins/<uuid:admin_id>/delete',views.DeleteGroupAdmin.as_view(),name='group_admin_delete'),
-    path('group/<uuid:group_id>/admins',views.GroupAdmins.as_view(),name='group_admins'),
+    # Group Admin
+    path('groups/<uuid:group_id>/admin/create',views.GroupAdmin.Create.as_view(),name='create_group_admin'),
+    path('groups/<uuid:group_id>/admins',views.GroupAdmin.List.as_view(),name='group_admins'),
+    path('groups/<uuid:group_id>/admins/<uuid:admin_id>/delete',views.GroupAdmin.Delete.as_view(),name='kick_group_admin'),
 ]
 
 
