dd588dbda2285d225e59a7e448c5e7e425d4addb
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-11 18:04:37 +0330

    refactor

--- core/serializers.py
+++ core/serializers.py
@@ -24,10 +24,8 @@ class BaseSerializer(serializers.Serializer):
     def is_valid(self, *, raise_exception=True):
         # raise exception default value True for raise custom style err
         try:
-            super(BaseSerializer, self).is_valid(raise_exception=raise_exception)
+            return super(BaseSerializer, self).is_valid(raise_exception=raise_exception)
         except Exception as e:
             # handle error with custom style
             if raise_exception:
                 raise exceptions.BadRequest(exceptions.get_errors_serializer(self))
-        else:
-            pass
