fd7b303ec7666708b4d4b707374ffa3de3bd42e9
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-11 18:04:16 +0330

    style

--- core/response.py
+++ core/response.py
@@ -1,9 +1,9 @@
 from rest_framework.response import Response as _Response
 
 
-def Response(data,errors=[],status=200,*args):
+def Response(data, errors=[], status=200, *args):
     return _Response({
-        "data":data,
-        "errors":errors,
-        "status":status
-    },status=status,*args)
+        "data": data,
+        "errors": errors,
+        "status": status
+    }, status=status, *args)
