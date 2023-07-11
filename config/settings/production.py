2e35ee81ebd2782eac34e1506f6be8217652fbcf
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-11 17:59:44 +0330

    add db in production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,3 +3,10 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['*'] # for now
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db_production.sqlite3',
+    }
+}
