d04cde2f63ddeb99dead7cef3dc8557ae1c2c960
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-13 11:35:38 +0330

    use mysql engine db

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ ALLOWED_HOSTS = ['*']  # for now
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
