c2e4741a7ae5df53eb4dfb784301a779299fd28d
Fazel Momeni <wwwfazel88@gmail.com>
2023-07-13 11:34:00 +0330

    use enironment var in production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,11 +2,15 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*'] # for now
+ALLOWED_HOSTS = ['*']  # for now
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db_production.sqlite3',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
